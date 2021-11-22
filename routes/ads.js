57e166ba72e242951ed7ebc27979257553f31a18
Johannes von Kessel <johannes.v.kessel@web.de>
2021-11-22 14:55:23 +0100

    ad list, creating, editing and deleting done

--- routes/ads.js
+++ routes/ads.js
@@ -1,5 +1,20 @@
 const router = require("express").Router();
+const { addListener } = require("../app");
 const Ad = require('../models/Ad')
+
+// List all ads
+
+router.get('/ads', (req, res, next) => {
+		
+	Ad.find()
+		.then(adsFromDB => {
+			
+			res.render('ads/index', { adList: adsFromDB })
+		})
+		.catch(err => next(err))
+});
+
+// Create new Ad
 
 router.get('/ads/add', (req, res, next) => {
 	res.render('ads/addForm')
@@ -8,9 +23,9 @@ router.get('/ads/add', (req, res, next) => {
 router.post('/ads', (req, res, next) => {
 	
 	const { title, category, time, date, address, description, price } = req.body
-	// console.log(title, description, author, rating)
+	
     console.log(req.body)
-	// create a new book
+	
 	Ad.create({
 	  title: title,
 	  category: category,
@@ -21,11 +36,68 @@ router.post('/ads', (req, res, next) => {
 	  price: price
 	})
 		.then(createdAd => {
-			console.log(createdAd)
-			// show the book details for the created book
-			// res.render('books/details', { book: createdBook })
+			
 			res.redirect(`/ads/${createdAd._id}`)
 		})
+        .catch(err => next(err))
 });
+
+// Display Ad
+
+router.get('/ads/:id', (req, res, next) => {
+	const id = req.params.id
+	Ad.findById(id)
+		.then(adFromDB => {
+			res.render('ads/details', { ad: adFromDB })
+		})
+		.catch(err => next(err))
+});
+
+// Edit Ad
+
+router.get('/ads/edit/:id', (req, res, next) => {
+	const id = req.params.id
+	Ad.findById(id)
+		.then(adFromDB => {
+			res.render('ads/editForm', { ad: adFromDB })
+		})
+		.catch(err => next(err))
+});
+
+router.post('/ads/edit/:id', (req, res, next) => {
+	const id = req.params.id
+	
+	const { title, category, time, date, address, description, price } = req.body
+	Ad.findByIdAndUpdate(id, {
+	  title,
+	  category,
+	  time,
+	  date,
+	  address,
+	  description,
+	  price,
+	}, { new: true })
+		.then(updatedAd => {
+			console.log(updatedAd)
+			
+			res.redirect(`/ads/${updatedAd._id}`)
+		})
+		.catch(err => next(err))
+});
+
+// Delete Ad
+
+router.get('/ads/delete/:id', (req, res, next) => {
+	const id = req.params.id
+	Ad.findByIdAndDelete(id)
+		.then(() => {
+			res.redirect('/ads')
+		})
+		.catch(err => {
+			next(err)
+		})
+});
+
+
 
 module.exports = router;
