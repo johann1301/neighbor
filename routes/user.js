0dc275eceea6472f5a52601cc33177a9d98388d2
Johannes von Kessel <johannes.v.kessel@web.de>
2021-11-24 12:35:20 +0100

    upload Profile picture - delete missing

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { addListener } = require("../app");
 const Ad = require('../models/Ad');
 const User = require("../models/User.model");
+const {uploader, cloudinary} = require("../config/cloudinary")
 
 // middleware to protect a route
 const loginCheck = () => {
@@ -23,21 +24,34 @@ const loginCheck = () => {
   
   });
 
-  router.post('/userProfile/edit', (req, res, next) => {
+  router.post('/userProfile/edit', uploader.single("profileImg"), (req, res, next) => {
 	const user = req.session.user;
-    const { street, number, zipcode, city } = req.body
+  const imgPath = req.file.path;
+  const imgName = req.file.originalname;
+  const publicId = req.file.filename;
+  const { street, number, zipcode, city } = req.body
+
+  // if (req.session.user.publicId !== req.file.filename){
+  //   cloudinary.uploader.destroy(publicId)
+  //   }
+
 	User.findByIdAndUpdate(user, {
-	 
+	
 	  address: {
         street: street,
         number: number,
         zipcode: zipcode,
         city: city,
     },
+    imgPath: imgPath,
+    imgName: imgName,
+    publicId: publicId,
 	  
-	}, { new: true })
+  }, { new: true })
 		.then(updatedUser => {
-                req.session.user = updatedUser
+      
+
+      req.session.user = updatedUser
                 
 			res.redirect("/userProfile")
 		})
