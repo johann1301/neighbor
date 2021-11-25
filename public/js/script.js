ae3c4da828a3b09ebc63af939bd36d9004df55ac
Johannes von Kessel <johannes.v.kessel@web.de>
2021-11-25 18:02:06 +0100

    done profile

--- public/js/script.js
+++ public/js/script.js
@@ -11,13 +11,16 @@ document.addEventListener(
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+if(signUpButton){
+  signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+  });
+}
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
 
+if(signInButton){
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
-
+}
 
